604cb4251b9d759b657c2e977ec04468bac55d04
heruiwoniou <heruiwoniou@qq.com>
2017-07-24 15:55:50 +0800

    'base'

---
+++ common/dispose.js
@@ -0,0 +1,106 @@
+/**
+ * Author:Herui/Administrator;
+ * CreateDate:2016/2/16
+ *
+ * Describe:
+ */
+
+var fs = require('fs')
+var mine = require('./ctype').types
+var base = require('ss-controller')
+var querystring = require('querystring')
+
+function Dispose (dir) {
+  this._dir = dir
+  this.cache = {}
+}
+Dispose.prototype = {
+  constructor: Dispose,
+  responseFile: function (realPath, request, response, pathname, ext) {
+    fs.exists(realPath, function (exists) {
+      if (!exists) {
+        response.writeHead(404, {
+          'Content-Type': 'text/plain'
+        })
+        response.write('This request URL ' + pathname + ' was not found on this server.')
+        response.end()
+      } else {
+        fs.readFile(realPath, 'binary', function (err, file) {
+          if (err) {
+            response.writeHead(500, {
+              'Content-Type': 'text/plain'
+            })
+            response.end(err)
+          } else {
+            var contentType = mine[ext] || 'text/plain'
+            response.writeHead(200, {
+              'Content-Type': contentType,
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Headers': 'X-Requested-With',
+              'Access-Control-Allow-Methods': 'GET,POST,OPTIONS'
+            })
+            response.write(file, 'binary')
+            response.end()
+          }
+        })
+      }
+    })
+  },
+  responseData: function (request, response, pathname, query, form, files) {
+    var paths = pathname.split('/'), controller
+    try {
+      if (paths.length > 3) throw new Error('URL Error !')
+      if (paths.length < 3) paths.push('index')
+
+      paths[0] == '' ? paths.shift() : null
+      if (this.cache[paths[0]] && false) {
+        controller = this.cache[paths[0]]
+      } else {
+        if (fs.existsSync(this._dir + '/controller/' + paths[0] + '.js')) {
+          try {
+            delete require.cache[require.resolve(this._dir + '/controller/' + paths[0] + '.js')]
+            controller = base.createController(require(this._dir + '/controller/' + paths[0] + '.js'))
+          } catch (loaderror) {
+            controller = base.createController()
+          }
+        } else {
+          controller = base.createController()
+        }
+
+        this.cache[paths[0]] = controller
+      }
+      var action = paths.pop()
+      controller[action] ? null : controller[action] = function () { return this.render.view() }
+      controller.controller = paths.join('/')
+      controller.action = action
+      controller.dir = this._dir
+      controller.querystring = querystring.parse(query)
+      controller.files = files
+      controller.form = form
+      controller.request = request
+      controller.response = response
+      var content = controller[action](controller.querystring, controller.form, files)
+      if (content !== undefined) {
+        if (content instanceof Promise) {
+          content.then(function (result) {
+            response.writeHead(200, { 'Content-Type': result.type })
+            response.write(result.data)
+            response.end()
+          })
+        } else {
+          response.writeHead(200, { 'Content-Type': content.type })
+          response.write(content.data)
+          response.end()
+        }
+      }
+    } catch (e) {
+      response.writeHead(500, { 'Content-Type': 'text/plain' })
+      response.write(e.code || e.message)
+      response.end()
+    }
+  }
+}
+
+module.exports = function (dir) {
+  return new Dispose(dir)
+}
---
+++ index.js
@@ -0,0 +1,83 @@
+/**
+ * Description:
+ * Author:Administrator
+ * CreateDateTime:2016/2/4.
+ */
+
+var multiparty = require('multiparty')
+var net = require('net')
+var opn = require('opn')
+var fs = require('fs')
+var cp = require('child_process')
+
+module.exports = {
+  run: function (dir, port, runtime) {
+    (function (p) {
+      var s = arguments.callee
+      var _server = net.createServer().listen(p)
+      _server.on('listening', function () {
+        _server.close()
+        new SimpleServer(dir, p, runtime)
+      })
+      _server.on('error', function () {
+        _server.close()
+        var oldport = p
+        var newport = ++p
+        console.log('端口:' + oldport + '已被占用,正在尝试端口:' + newport)
+        s(newport)
+      })
+    })(port)
+  }
+}
+
+function SimpleServer (dir, port, runtime) {
+  this._dir = dir.trim('/')
+  this._port = port || 3000
+  this._runtime = runtime
+
+  this.initialize()
+}
+
+SimpleServer.prototype = {
+  constructor: SimpleServer,
+  initialize: function () {
+    var port = this._port
+    var dir = this._dir
+    var runtime = this._runtime || ''
+    var http = require('http')
+    var url = require('url')
+    var path = require('path')
+    var dispose = require('./common/dispose')(this._dir)
+
+    var server = http.createServer(function (request, response) {
+      var uri = url.parse(request.url)
+      var pathname = uri.pathname
+      var query = uri.query
+      if (pathname.charAt(pathname.length - 1) === '/') {
+        pathname += 'index.html'
+      }
+      var realPath = path.join(dir + '/' + runtime, pathname)
+      var ext = path.extname(realPath)
+      ext = ext ? ext.slice(1) : 'unknown'
+      console.log(realPath)
+      if (request.method === 'POST') {
+        var from = new multiparty.Form()
+        from.parse(request, function (err, fields, files) {
+          dispose.responseData(request, response, pathname, query, fields, files)
+        })
+      } else { ext === 'unknown' ? dispose.responseData(request, response, pathname, query) : dispose.responseFile(realPath, request, response, pathname, ext) }
+    })
+    server.listen(port)
+    console.log('Server runing at port: ' + port + '.')
+    // var exec = cp.exec,
+    //   opened = false
+
+    opn('http://127.0.0.1:' + port)
+    // if (fs.existsSync('c:/Program Files (x86)/Google/Chrome/Application/chrome.exe')) {
+    //   exec('start "" "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" http://localhost:' + port)
+    //   opened = true
+    // } else if (fs.existsSync('c:/Program Files/Google/Chrome/Application/chrome.exe') && !opened) {
+    //   exec('start "" "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" http://localhost:' + port)
+    // }
+  }
+}
